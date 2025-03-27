9d1dead3fa7d2f98d45e07fb1eb29d7df07a7c6c
cassiatorczon <cassiatorczon@gmail.com>
2025-03-27 15:30:34 -0400

    class week11

---
+++ week11/class/Examples.hs
@@ -0,0 +1,53 @@
+module Examples where
+
+---
+
+ex1 :: IO ()
+ex1 = putStr "Hello, " >> putStr "world!"
+
+ex2 :: IO Int
+ex2 = putStr "Enter a number: " >> readLn
+
+ex3 :: IO ()
+ex3 =
+  putStr "Enter a number: "
+    >> readLn
+    >>= (\n -> print (n + 1))
+
+ex4 :: IO Int
+ex4 =
+  getLine
+    >>= (\input -> return (length input))
+
+---
+
+ex1' :: IO ()
+ex1' = do
+  putStr "Hello, "
+  putStr "world!"
+
+ex2' :: IO Int
+ex2' = do
+  putStr "Enter a number: "
+  readLn
+
+ex3' :: IO ()
+ex3' = do
+  putStr "Enter a number: "
+  n <- readLn
+  print (n + 1)
+
+ex4' :: IO Int
+ex4' = do
+  input <- getLine
+  return (length input)
+
+ex5 :: IO ()
+ex5 = do
+  putStr "Give me five: "
+  n <- readLn
+  if n == 5
+    then putStrLn "Hooray!"
+    else do
+      putStrLn "Try again."
+      ex5
---
+++ week11/class/Exercises.hs
@@ -0,0 +1,23 @@
+module Exercises where
+
+{- Exercise:
+Ask the user for two numbers and print their sum.
+Make sure to check that it works using the terminal. -}
+
+addTwo :: IO ()
+addTwo = error "unimplemented"
+
+{- Exercise:
+Given an integer [n], ask the user to repeatedly
+guess a number until they get [n]. Tell them if their
+guess is higher or lower than expected. If they get
+the right answer, tell them so and end the game. -}
+
+guessGame :: Int -> IO ()
+guessGame = error "unimplemented"
+
+{- Exercise:
+Have a classmate test out your game in the terminal.
+See if you can find any bugs in the one you test!
+(For this exercise, it's OK to crash on non-integer
+inputs.) -}
